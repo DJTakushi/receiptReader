cba88dfc93176c3606d6ef9d3b4ff1c30aa29a39
DJTakushi <okinawanspud@gmail.com>
2023-01-14 22:46:51 +0000

    paramerize filepath

--- examples/tesseract/basicExample.cpp
+++ examples/tesseract/basicExample.cpp
@@ -1,8 +1,14 @@
 #include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
+#include <iostream>
+int main(int argc, char* argv[]){
+    std::string filepath = "/receiptReader/examples/ehiorobo//test_image.jpg";
+    if(argc == 1){
+        std::cout << "using default input file:"<<filepath<<std::endl;}
+    else{
+        filepath = std::string(argv[1]);
+    }
 
-int main()
-{
     char *outText;
 
     tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
@@ -13,8 +19,7 @@ int main()
     }
 
     // Open input image with leptonica library
-    Pix *image = pixRead("/usr/src/tesseract/testing/phototest.tif");
-    // Pix *image = pixRead("/receiptReader/examples/ehiorobo/test_image.jpg");
+    Pix *image = pixRead(filepath.c_str());
     api->SetImage(image);
     // Get OCR result
     outText = api->GetUTF8Text();
